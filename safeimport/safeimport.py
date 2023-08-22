2b72db636bed70d0c580ca73b59960f29660b8d9
apri-me <afrzi@proton.me>
2023-08-22 08:43:59 +0330

    bug fix

--- safeimport/safeimport.py
+++ safeimport/safeimport.py
@@ -2,15 +2,18 @@ from contextlib import contextmanager
 
 
 @contextmanager
-def safe_import(depth: int):
-    """When your base is <depth> times outer your script (depth >= 1)"""
+def safe_import(depth: int, file_path: str):
+    """When your base is <depth> times outer your script (depth >= 0)
+    file_abs_path can be achieved from __file__ variable"""
     if type(depth) != type(0) or depth < 1:
-        raise Exception("Depth should be integer and greater than zero!")
+        raise Exception("Depth should be integer and greater than or equal to zero!")
     try:
         import os
         import sys
-        ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "/".join([".."] * (depth + 1))))
+        print(__file__)
+        ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(file_path), "/".join([".."] * (depth))))
         sys.path.append(ROOT_DIR)
         yield 
     finally:
         del ROOT_DIR, os, sys
+
